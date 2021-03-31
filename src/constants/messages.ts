11c476dbb573c3b308c1e1e02e954247d9da68f9
Luis Fernando Prudencio Cruz <lfernando1120@gmail.com>
2021-03-31 01:23:22 -0600

    fix: ajuste de mensajes

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,9 +1,9 @@
 export enum Messages {
     NOT_FOUND_ENDPOINT = 'No se encontró el endpoint',
     OPERATION_OK = 'Operación exitosa',
-    ADD_ACCESS_OK = 'El acceso de agregó correctamente',
-    UPDATE_ACCESS_OK = 'El acceso de actualizó correctamente',
-    DELETE_ACCESS_OK = 'El acceso de eliminó correctamente',
+    ADD_ACCESS_OK = 'El acceso se agregó correctamente',
+    UPDATE_ACCESS_OK = 'El acceso se actualizó correctamente',
+    DELETE_ACCESS_OK = 'El acceso se eliminó correctamente',
     GET_ACCESS_ERROR = 'Ocurrió un error al obtener los accesos',
     ADD_ACCESS_ERROR = 'Ocurrió un error al agregar el acceso',
     ACCESS_NOT_FOUND = 'No se encontró el acceso',
