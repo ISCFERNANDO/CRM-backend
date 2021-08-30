58c5c1a2eea85b25ed3a72ec7d3fc7e889d8d343
fernando <lfernando1120@gmail.com>
2021-08-30 16:35:19 -0500

    fix: Fixed typeorm connection with mysql

--- src/utils/db_connection.ts
+++ src/utils/db_connection.ts
@@ -1,14 +1,23 @@
+import { join } from 'path';
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import { getConnectionManager } from 'typeorm';
 
-export default createConnection({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_HOST ? parseInt(process.env.DB_HOST) : 3306,
-    username: process.env.DB_USER_NAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [__dirname + '../entities'],
-    synchronize: true,
-    logging: false
-});
+const connectionManager = getConnectionManager();
+
+const createMysqlConnection = () => {
+    return connectionManager
+        .create({
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+            username: process.env.DB_USER_NAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [join(__dirname, '..', '/entities/**.*s')],
+            synchronize: false,
+            logging: true
+        })
+        .connect();
+};
+
+export { createMysqlConnection };
