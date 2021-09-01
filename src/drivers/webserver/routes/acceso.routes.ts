48c00b11dd342a2e71b0bb15ac0d6e246c0c7a02
fernando <lfernando1120@gmail.com>
2021-09-01 12:34:23 -0500

    fix: fixed access routes

--- src/drivers/webserver/routes/acceso.routes.ts
+++ src/drivers/webserver/routes/acceso.routes.ts
@@ -4,22 +4,24 @@ import Container from 'typedi';
 import { AccessoController } from '../../../controllers/acceso.controller';
 import createAccessvalidator from '../../../validators/create-acceso.validator';
 
-const controller = Container.get(AccessoController);
+const initializeAccesoRoute = () => {
+    const controller = Container.get(AccessoController);
 
-const accesoRoute = Router();
-accesoRoute
-    .route('/v1/access')
-    .get((req, res, next) => controller.getAllAccess(req, res, next))
-    .post(createAccessvalidator, (req, res, next) =>
-        controller.addAccess(req, res, next)
-    )
-    .delete((req, res, next) => controller.deleteAccessess(req, res, next));
-accesoRoute
-    .route('/v1/access/:id')
-    .get((req, res, next) => controller.findById(req, res, next))
-    .put(createAccessvalidator, (req, res, next) =>
-        controller.updateAccess(req, res, next)
-    )
-    .delete((req, res, next) => controller.deleteAccess(req, res, next));
-
-export default accesoRoute;
+    const accesoRoute = Router();
+    accesoRoute
+        .route('/v1/access')
+        .get((req, res, next) => controller.getAllAccess(req, res, next))
+        .post(createAccessvalidator, (req, res, next) =>
+            controller.addAccess(req, res, next)
+        )
+        .delete((req, res, next) => controller.deleteAccessess(req, res, next));
+    accesoRoute
+        .route('/v1/access/:id')
+        .get((req, res, next) => controller.findById(req, res, next))
+        .put(createAccessvalidator, (req, res, next) =>
+            controller.updateAccess(req, res, next)
+        )
+        .delete((req, res, next) => controller.deleteAccess(req, res, next));
+    return accesoRoute;
+};
+export { initializeAccesoRoute };
